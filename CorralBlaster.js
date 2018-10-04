9232bec5f90a10217a4c5b4d2313327e54a00482
Anna Nienhaus <anienhaus@mymail.mines.edu>
2018-10-03 19:26:12 -0600

    Added functions to draw hexagon grid

--- CorralBlaster.js
+++ CorralBlaster.js
@@ -9,10 +9,21 @@ function drawBoard() {
 	ctx.fillRect(0, 0, canvas.width, canvas.height);
 
 	// Draw hexagon grid
-	drawHex(40, 40, 20);
+	var numRows = 13;
+	var numCols = 12;
+	var startX = 40;
+	var startY = 20;
+	var sideLen = 20;
+	var i;
+	for (i = 0; i < numRows; i++) {
+		drawRow(numCols, startX, startY + i*(sideLen * Math.sqrt(3)), sideLen);
+	}
 }
 
-// Draws a hexagon with its top left corner at the given X and Y with the given side length
+// Draws a single hexagon
+// X: x position for the top left corner
+// Y: y position for the top left corner
+// sideLen: length of each side of the hexagon
 function drawHex(X, Y, sideLen) {
 
 	var canvas = document.getElementById("canvas");
@@ -63,4 +74,19 @@ function drawHex(X, Y, sideLen) {
 	Y = Y - sideLen/2 * Math.sqrt(3);
 	ctx.lineTo(X, Y);
 	ctx.stroke();
+}
+
+// Draws two rows of hexagons (offset)
+// numHexes: Number of hexagons per one row
+// startX: top left x position for first hexagon
+// startY: top left y position for first hexagon
+// sideLen: length of one side of a hexagon
+function drawRow(numHexes, startX, startY, sideLen) {
+	var i;
+	for (i = 0; i < numHexes; i++) {
+		drawHex(startX + i*3*sideLen, startY, sideLen);
+	}
+	for (i = 0; i < numHexes; i++) {
+		drawHex(startX + 1.5*sideLen + i*3*sideLen, startY + sideLen/2 * Math.sqrt(3), sideLen);
+	}
 }
